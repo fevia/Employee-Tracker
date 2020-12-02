6c62dce22df08775fa6be8abc8eb143deba8b968
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-01 19:21:41 -0600

    inquirer and mysql connection

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,11 @@
 -- starting data for department -- 
 INSERT INTO department (name) VALUES ("Manufacturing");
 INSERT INTO department (name) VALUES ("Research and Development");
+INSERT INTO department (name) VALUES ("Sales");
 -- starting data for role --  
-INSERT INTO role (title, salary, department_id) VALUES ("Technician", 80000, 153);
-INSERT INTO role (title, salary, department_id) VALUES ("Scientist", 95000, 486);
+INSERT INTO role (title, salary, department_id) VALUES ("Technician", 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Scientist", 95000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Director", 100000, 3);
 -- starting data for employee -- 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brian", "Choi", 123, 007);
-INSERT INTO role (first_name, last_name, role_id, manager_id) VALUES ("Francis", "Villar", 124, 007);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brian", "Choi", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Francis", "Villar", 3, null);
